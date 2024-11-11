edf86a4aca4cf2e65d742346cd9273d1c1a545ab
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-11-10 19:50:07 -0800

    add footer to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
