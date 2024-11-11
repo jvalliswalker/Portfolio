d289481e4d965e03959d2c8a5f7ed38ddcc25041
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-11-10 19:49:38 -0800

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,17 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Navbar from "react-bootstrap/Navbar";
+
+import "../styles/index.css";
+
 function Footer() {
-  return <div>Footer placeholder</div>;
+  return (
+    <div className="customFooter">
+      <Navbar className="bg-primary" fixed="bottom">
+        <Container fluid>Footer placeholder</Container>
+      </Navbar>
+    </div>
+  );
 }
 
 export default Footer;
