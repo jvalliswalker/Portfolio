929556b6fca543f0f74a6c359dabab8d6b66b141
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-11-10 19:49:28 -0800

    build out preliminary nav bar and links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
 import { useState } from "react";
+// import { Link } from "react-router-dom";
+import Navigation from "./Navigation.jsx";
+import Nav from "react-bootstrap/Nav";
+
+import "../styles/index.css";
+
+const links = [
+  <Nav.Link key={"home"} href="/">
+    Home
+  </Nav.Link>,
+  <Nav.Link key={"about"} href="/About">
+    About
+  </Nav.Link>,
+];
 
 function Header() {
-  return <div className="search-bar ui segement"></div>;
+  return (
+    <div className="search-bar ui segement customHeader">
+      <Navigation links={links} />
+    </div>
+  );
 }
 
 export default Header;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,20 @@
-function Navigation() {
-  return <div>Navigation placeholder</div>;
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+// import NavDropdown from "react-bootstrap/NavDropdown";
+
+function Navigation({ links }) {
+  return (
+    <Navbar bg="primary" expand="lg" fixed="top">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">{links.map((link) => link)}</Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Navigation;
