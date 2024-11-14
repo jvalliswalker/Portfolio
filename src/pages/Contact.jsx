73cf3b15de9b420ba29c8cb47766611c7644b7f7
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-11-13 19:58:26 -0800

    build out basic form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,38 @@
+import { useOutletContext } from "react-router";
+
 function Contact() {
-  return <div>Contact placeholder</div>;
+  const [styleHandler] = useOutletContext();
+
+  const style = styleHandler.contact;
+
+  return (
+    <div>
+      <div class="mb-3 fw-bold fs-2">Contact Me</div>
+      <form>
+        <div class="mb-3">
+          <label for="contactInputName" class="form-label">
+            Name
+          </label>
+          <input type="email" class="form-control" id="contactInputName" />
+        </div>
+        <div class="mb-3">
+          <label for="contactInputEmail" class="form-label">
+            Email
+          </label>
+          <input type="email" class="form-control" id="contactInputEmail" />
+        </div>
+        <div class="mb-3">
+          <label class="form-label" for="contactInputMessage">
+            Message
+          </label>
+          <textarea class="form-control" id="contactInputMessage" rows="4" />
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
