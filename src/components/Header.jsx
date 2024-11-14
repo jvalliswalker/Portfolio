66edf3b75c4c852c63d0a26e720054794e4952f4
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-11-13 17:43:13 -0800

    Remove unneeded import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-// import { Link } from "react-router-dom";
 import Navigation from "./Navigation.jsx";
 import Nav from "react-bootstrap/Nav";
 import "../styles/index.css";
