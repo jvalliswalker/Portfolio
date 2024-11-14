891b06fff0d1db390e6da8f9d9612ff98e6a24f1
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-11-13 17:44:49 -0800

    Add url navigation, expand styling

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,35 @@
-function Project({ img, title, url, github }) {
+function Project({ img, title, url, github, styling }) {
+  function navigateTo(url) {
+    window.open(url, "_blank").focus();
+  }
+
   return (
-    <div>
-      <img src={img} className="img-thumbnail" />
-      <div>{title}</div>
-      <div>{url}</div>
-      <div>{github}</div>
+    <div className="project-image-container">
+      <img src={img} className={styling.projectImageStyling} />
+      <div className={styling.projectMiddleStyling}>
+        <div className="container">
+          <div className="row mb-3">
+            <div className={styling.projectImageTitleStyling}>{title}</div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div
+                onClick={() => navigateTo(url)}
+                className={styling.projectButtonStyling}
+              >
+                Visit Site
+              </div>
+            </div>
+            <div className="col">
+              <img
+                src="\images\icon-github.png"
+                onClick={() => navigateTo(github)}
+                className={styling.projectGithubIconStyling}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
