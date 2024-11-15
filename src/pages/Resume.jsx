e0b0425e038a7f52f1689580378afbaffc256263
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-11-14 21:41:54 -0800

    Build out resume (WIP)

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,14 @@
 function Resume() {
-  return <div>Resume placeholder</div>;
+  return (
+    <>
+      <div>list of the developer’s proficiencies</div>
+      <div>
+        <a href="https://drive.google.com/uc?export=download&id=10taFQdyCTgh81d3MzmV5yoNwwlvTcCWk">
+          Download Resume
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default Resume;
