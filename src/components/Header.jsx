7ff729052e8f35b6b0dca901cd6471898eeda342
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-12-31 15:45:15 -0800

    update links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,11 @@ import Nav from "react-bootstrap/Nav";
 import "../styles/index.css";
 
 const links = [
-  <Nav.Link key={"about"} href="/">
-    About Me
-  </Nav.Link>,
   <Nav.Link key={"portfolio"} href="/portfolio">
     Portfolio
   </Nav.Link>,
-  <Nav.Link key={"contact"} href="/contact">
-    Contact
+  <Nav.Link key={"about"} href="/about">
+    About Me
   </Nav.Link>,
   <Nav.Link key={"resume"} href="/resume">
     Resume
