5b1874079e09c2ec20da1d994339aaa0ae639ba5
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2025-01-02 15:02:22 -0800

    update contact page (WIP)

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,21 +3,59 @@ import { useOutletContext } from "react-router";
 function Contact() {
   const [styleHandler] = useOutletContext();
 
-  const style = styleHandler.contact;
+  function navigateTo(url) {
+    window.open(url, "_blank").focus();
+  }
 
   return (
-    <div className={style.formWrapperStyling}>
-      <div className="mb-3 fw-bold fs-2">Contact Me</div>
-      <div>
-        <div>
-          The best way to get in touch with me is through{" "}
-          <a
-            href="https://www.linkedin.com/in/jamil-vallis-walker-43706a60/"
-            target="_blank"
-          >
-            LinkedIn
-          </a>{" "}
-          .
+    <div>
+      <h2 className={styleHandler.underlinedHeader}>Let's Get In Touch</h2>
+      <div className="mb-3">
+        <p className="mb-5">
+          Thanks so much for your interest. Below are links to my sites and my
+          resume.
+        </p>
+        <div className="row">
+          <div className={styleHandler.siteWrapper}>
+            <div
+              className={styleHandler.iconWrapper}
+              onClick={() =>
+                navigateTo(
+                  "https://www.linkedin.com/in/jamil-vallis-walker-43706a60/"
+                )
+              }
+            >
+              <img
+                src="/images/icon-linkedin-blue.png"
+                alt="Blue Linkedin Icon"
+                className="icon-linkedin-blue mb-2"
+              />
+              <div>LinkedIn</div>
+            </div>
+          </div>
+          <div className={styleHandler.siteWrapper}>
+            <div
+              className={styleHandler.iconWrapper}
+              onClick={() => navigateTo("https://github.com/jvalliswalker")}
+            >
+              <img
+                src="/images/icon-github.png"
+                className="icon-linkedin-blue mb-2"
+              />
+              <div>Github</div>
+            </div>
+          </div>
+          <div className={styleHandler.siteWrapper}>
+            <div
+              className={styleHandler.iconWrapper}
+              onClick={() =>
+                navigateTo("/resume/Resume%20-%20J%2E%20Vallis-Walker.pdf")
+              }
+            >
+              <img src="/images/icon-resume.png" className="icon-resume mb-1" />
+              <div>Resume PDF</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
